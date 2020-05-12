4544303e60168ecc273e82f46fa0ac1df2bcb3ab
Derek Wang <wang.y.derek@gmail.com>
2020-05-12 00:45:47 -0400

    progress on website data processing

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ class WikiGatherer:
 
     def __init__(self, url):
         self.url = url
-        self.links = set()
+        self.links = []
+        self.bad = []
 
     def wikipedia_crawler(self):
-
         source_code = requests.get(self.url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, features="lxml")
@@ -21,16 +21,24 @@ class WikiGatherer:
             if href[:2] == '//':
                 href = href[2:]
             print(str(count) + ". " + href + '\n')
-            self.links.add(href)
+            self.links.append(href)
             count += 1
         # print(len(sources))
 
     def get_single_site_data(self):
+        count = 0
         for link in self.links:
-            source_code = requests.get(link)
-            plain_text = source_code.text
-            soup = BeautifulSoup(plain_text, features="lxml")
+            try:
+                source_code = requests.get(link)
+                plain_text = source_code.text
+                soup = BeautifulSoup(plain_text, features="lxml")
+                print(str(count) + ". " + link)
+            except:
+                print(link)
+            count += 1
 
 
-if __name__ == '__main':
-    crawl = WikiGatherer(URL)
+
+crawl = WikiGatherer(URL)
+crawl.wikipedia_crawler()
+crawl.get_single_site_data()
